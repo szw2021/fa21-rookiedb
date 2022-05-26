02d852a72b776519aaf11d66ef1343da4bd07c2c
shizhiwei <szw2016run@gmail.com>
2022-05-26 17:29:27 +0800

    finish LockType

--- src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
+++ src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
@@ -11,6 +11,13 @@ public enum LockType {
     SIX, // shared intention exclusive
     NL;  // no lock held
 
+    private static final boolean[][] lockTypeMatrix= {{true, true, true, true, false},
+            {true, true, false, false, false},
+            {true, false, true, false, false},
+            {true, false, false, false, false},
+            {false, false, false, false, false}
+    };
+
     /**
      * This method checks whether lock types A and B are compatible with
      * each other. If a transaction can hold lock type A on a resource
@@ -22,10 +29,38 @@ public enum LockType {
             throw new NullPointerException("null lock type");
         }
         // TODO(proj4_part1): implement
+        if (a == NL || b == NL) {
+            return true;
+        }
 
-        return false;
+        return getCompatibleHelper(a, b);
+
     }
 
+    private static boolean getCompatibleHelper(LockType a, LockType b) {
+        int index1 = getLockTypeIndex(a);
+        int index2 = getLockTypeIndex(b);
+        return lockTypeMatrix[index1][index2];
+    }
+
+    private static int getLockTypeIndex(LockType a) {
+        if (a == IS) {
+            return 0;
+        }
+        if (a == IX) {
+            return 1;
+        }
+        if (a == S) {
+            return 2;
+        }
+        if (a == SIX) {
+            return 3;
+        }
+        if (a == X) {
+            return 4;
+        }
+        return -1;
+    }
     /**
      * This method returns the lock on the parent resource
      * that should be requested for a lock of type A to be granted.
@@ -55,6 +90,22 @@ public enum LockType {
         }
         // TODO(proj4_part1): implement
 
+        if (parentLockType.equals(childLockType) || childLockType == NL) {
+            return true;
+        }
+
+        if (parentLockType == NL) {
+            return false;
+        }
+
+        if (childLockType == S || childLockType == IS) {
+            return parentLockType == IS || parentLockType == IX;
+        }
+
+        if (childLockType == X || childLockType == IX || childLockType == SIX) {
+            return parentLockType == IX || parentLockType == SIX;
+        }
+
         return false;
     }
 
@@ -69,6 +120,26 @@ public enum LockType {
             throw new NullPointerException("null lock type");
         }
         // TODO(proj4_part1): implement
+
+        if (substitute == required || required == NL) {
+            return true;
+        }
+
+        if (required == S) {
+            return substitute == SIX || substitute == X;
+        }
+
+        if (required == IS) {
+            return substitute == SIX || substitute == IX || substitute == S || substitute == X;
+        }
+
+        if (required == IX) {
+            return substitute == X || substitute == SIX;
+        }
+
+        if (required == SIX) {
+            return substitute == X;
+        }
 
         return false;
     }
